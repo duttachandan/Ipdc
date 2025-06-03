fa30012c766fb33cc38ddf15d75dc39a0533e851
duttachandan <duttaankit917@gmail.com>
2025-06-03 18:57:46 +0530

    updated navbar

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
-// Initializing AOS 
+// Initializing AOS
 AOS.init();
+
+// Vanila Js
+
+const navCollapse = document.querySelector(".nav-collapse");
+
+const navToggler = document.querySelector(".nav-toggler");
+
+navToggler.addEventListener("click", () => {
+  navCollapse.classList.toggle("active");
+});
