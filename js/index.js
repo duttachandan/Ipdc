9fbb75237c6d25926f64794a7f83f4fb12b14723
Chandan Dutta <duttaankit917@gmail.com>
2025-06-15 23:19:40 +0530

    update animation pause, aos timing function, position etc

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
-// Initializing AOS
-AOS.init();
-
-// Vanila Js
-
-const navCollapse = document.querySelector(".nav-collapse");
-
-const navToggler = document.querySelector(".nav-toggler");
-
-navToggler.addEventListener("click", () => {
-  navCollapse.classList.toggle("active");
+// Initializing AOS 
+AOS.init({
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 });
